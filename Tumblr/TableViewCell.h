14a38a6d31ef6293c158cdacaa853262a00712cd
Tomisin Ogunfunmi <tomisin@fb.com>
2021-06-24 11:18:17 -0500

    Initial Commit

---
+++ Tumblr/TableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.h
+//  Tumblr
+//
+//  Created by tomisin on 6/24/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoView;
+
+@end
+
+NS_ASSUME_NONNULL_END
